71b06376d453228243c92712568bc0d2a31a192a
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-03-29 20:46:38 -0300

    updated

---
+++ src/containers/Register/index.js
@@ -0,0 +1,104 @@
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as Yup from 'yup'
+import api from '../../services/api'
+
+import Logo from '../../assets/login/logo-codeburger.svg'
+
+import RegisterImg from '../../assets/login/image-cadastro-burger.svg'
+
+import Button from '../../components/Button'
+
+import {
+  Container,
+  RegisterImage,
+  ContainerItens,
+  H1,
+  Label,
+  Input,
+  ErrorMessage,
+  LogarLink
+} from './styles'
+
+function Register () {
+  const schema = Yup.object().shape({
+    name: Yup.string().required('O seu nome é obrigatório'),
+    email: Yup.string()
+      .email('Digite um email válido')
+      .required('O email é obrigatório'),
+    password: Yup.string()
+      .required('A senha é obrigatória')
+      .min(6, 'A senha deve ter pelo menos 6 digítos'),
+    confirmPassword: Yup.string()
+      .required('A senha é obrigatória')
+      .oneOf([Yup.ref('password')],'As senhas devem ser iguais')
+  })
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    resolver: yupResolver(schema)
+  })
+
+  const onSubmit = async clientData => {
+    const response = await api.post('/users', {
+      name: clientData.name,
+      email: clientData.email,
+      password: clientData.password
+    })
+    console.log(response)
+  }
+
+  return (
+    <Container>
+      <RegisterImage src={RegisterImg} alt='imagem hamburger' />
+      <ContainerItens>
+        <img src={Logo} alt='logo-codeburger' />
+        <H1>Cadastre-se</H1>
+        <form noValidate onSubmit={handleSubmit(onSubmit)}>
+          <Label error={errors.name?.message}> Nome</Label>
+          <Input
+            type='text'
+            {...register('name')}
+            error={errors.name?.message}
+          />
+          <ErrorMessage> {errors.name?.message}</ErrorMessage>
+
+          <Label error={errors.email?.message}> Email</Label>
+          <Input
+            type='email'
+            {...register('email')}
+            error={errors.email?.message}
+          />
+          <ErrorMessage> {errors.email?.message}</ErrorMessage>
+
+          <Label error={errors.password?.message}> Senha</Label>
+          <Input
+            type='password'
+            {...register('password')}
+            error={errors.password?.message}
+          />
+          <ErrorMessage> {errors.password?.message}</ErrorMessage>
+
+          <Label error={errors.confirmPassword?.message}>Confirme Senha</Label>
+          <Input
+            type='password'
+            {...register('confirmPassword')}
+            error={errors.confirmPassword?.message}
+          />
+          <ErrorMessage> {errors.confirmPassword?.message}</ErrorMessage>
+
+          <Button type='submit' style={{ marginTop: 20, marginLeft: 104 }}>
+            Entrar
+          </Button>
+        </form>
+        <LogarLink>Já possui conta? Entre</LogarLink>
+      </ContainerItens>
+    </Container>
+  )
+}
+
+export default Register
---
+++ src/containers/Register/styles.js
@@ -0,0 +1,89 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const RegisterImage = styled.img`
+  max-height: 110vh;
+`
+
+export const ContainerItens = styled.div`
+  background-color: #373737;
+  width: 100vw;
+  height: 100vh;
+  padding: 25px 75px;
+  flex-direction: column;
+  display: flex;
+  justify-content: center;
+
+  img {
+    width: 338px;
+    height: 125px;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+  }
+`
+export const H1 = styled.h1`
+  font-style: normal;
+  font-weight: 500;
+  font-size: 30px;
+  line-height: 28px;
+  color: #ffffff;
+  text-align: center;
+  margin-top: 40px;
+`
+
+export const Label = styled.p`
+  margin-top: ${props => (props.error ? '10px' : '17px')};
+  margin-bottom: 5px;
+  margin-left: 20px;
+
+  font-style: normal;
+  font-weight: 500;
+  font-size: 15px;
+  line-height: 14px;
+
+  color: #ffffff;
+`
+
+export const Input = styled.input`
+  width: 390px;
+  height: 38px;
+
+  background: #ffffff;
+  box-shadow: 3px 3px 10px rgba(74, 144, 226, 0.19);
+  border-radius: 5px;
+  outline: none;
+  border: ${props => (props.error ? '2px solid #cc1717' : 'none')};
+`
+
+export const ErrorMessage = styled.p`
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  margin-top: 5px;
+  margin-left: 5px;
+  color: #cc1717;
+`
+
+
+export const LogarLink = styled.p`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 16px;
+
+  margin-left: 110px;
+  margin-top: 32px;
+  cursor: pointer;
+
+  color: #ffffff;
+`
