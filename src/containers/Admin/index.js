e2aa597cb1c2e61bcacc1d2f47cdce5250e28ce0
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-04-02 14:40:24 -0300

    created Admin Page

---
+++ src/containers/Admin/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Container } from './styles'
+
+import Orders from './Orders'
+
+export function Admin () {
+    return (
+        <Container>
+            <h1>Admin</h1>
+            <Orders />
+        </Container>
+    )
+}
+
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
 
-import { Login, Register, Home, Products, Cart } from '../containers'
+import { Login, Register, Home, Products, Cart, Admin } from '../containers'
 import PrivateRoute from '../routes/private-routes'
 
 function AppRoutes () {
@@ -13,6 +13,8 @@ function AppRoutes () {
         <PrivateRoute exact path='/' component={Home} />
         <PrivateRoute exact path='/produtos' component={Products} />
         <PrivateRoute exact path='/carrinho' component={Cart} />
+        <Route exact path='/pedidos' component={Admin} />
+
       </Switch>
     </Router>
   )
