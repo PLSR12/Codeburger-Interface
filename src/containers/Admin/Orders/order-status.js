59d6af53ee5e8d20d03bceba5d2c918cb00ba3e3
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-04-06 12:11:41 -0300

    updated

---
+++ src/containers/Admin/Orders/order-status.js
@@ -0,0 +1,10 @@
+const status = [
+  { id: 1, value: 'Todos', label: 'Todos' },
+  { id: 2, value: 'Pedido realizado', label: 'Realizado ' },
+  { id: 3, value: 'Em preparação ', label: 'Preparando ' },
+  { id: 4, value: 'Pedido Pronto ', label: 'Pronto ' },
+  { id: 5, value: 'A caminho ', label: 'A caminho ' },
+  { id: 6, value: 'Pedido entregue', label: 'Entregue ' }
+]
+
+export default status
--- src/containers/Admin/Orders/styles.js
+++ src/containers/Admin/Orders/styles.js
@@ -1,3 +1,34 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+import ReactSelect from 'react-select'
+
+export const Container = styled.div`
+  background-color: #efefef;
+  min-height: 100vh;
+  padding: 30px;
+`
+
+export const ReactSelectStyle = styled(ReactSelect)`
+  width: 250px;
+
+  .css-1s2u09g-control {
+    cursor: pointer;
+  }
+`
+export const Menu = styled.div`
+  display: flex;
+  width: 100%;
+  flex-wrap: wrap;
+  gap: 7vh;
+  justify-content:center;
+  margin: 20px 0;
+
+`
+export const LinkMenu = styled.a`
+  color: #323D5D;
+  cursor: pointer;
+  font-size: 1rem;
+  font-weight: ${props =>(props.isActiveStatus ? 'bold' : 'normal')};
+  border-bottom: ${props => props.isActiveStatus && '3px solid #9758A6'};
+  padding-bottom: 4px;
+`
